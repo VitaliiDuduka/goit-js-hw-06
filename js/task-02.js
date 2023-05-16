412c23019c5634984333795d7547435887b9fe5b
Vitalii Dududka <vitalyd_2001@yahoo.com>
2023-05-17 00:01:01 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const components = ingredients.map(ingredient => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = [ingredient];
+  return liEl;
+});
+
+document.querySelector("#ingredients").append(...components);
+
