a657a67815fb1d9e37cded66049475e478d2399c
Vitalii Dududka <vitalyd_2001@yahoo.com>
2023-05-19 10:36:37 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const refs = {
+  color: document.querySelector('.widget'),
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+}
+
+refs.button.addEventListener('click', () => {
+  refs.span.textContent = getRandomHexColor();
+  refs.color.parentNode.style.backgroundColor = getRandomHexColor();
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
